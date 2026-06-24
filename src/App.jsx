02bce93c04897131292e241ee71030bf96224263
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-24 18:14:13 +0500

    feat: implement food data state management and integrate search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useState } from "react";
+import Search from "./components/Search";
 
 export default function App() {
+  const [foodData,setFoodData] = useState([]);
   return (
-    <div>App</div>
+    <div>App
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      {foodData.map((food)=>(
+        <h1 key={food.id}>{food.title}</h1>
+      ))}
+    </div>
   )
 }
