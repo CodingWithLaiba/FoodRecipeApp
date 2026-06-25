e6d08afa9c377b7fee48e6113f8ccd875d336056
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-25 14:24:58 +0500

    feat: enhance FoodItem component with layout and functionality improvements

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,9 +1,16 @@
-export default function FoodItem({ food }) {
+export default function FoodItem({ food,setFoodId }) {
   return (
-    <div>
-        <img src={food.image} alt="" />
-      <h1>{food.title}</h1>
-      <button>View Recipe</button>
+    <div className="w-80 rounded-lg shadow-xl overflow-hidden m-5 font-sans">
+      <img className="max-w-full h-auto" src={food.image} alt="" />
+      <div className="font-sans text-center ">
+        <p className="font-semibold text-lg">{food.title}</p>
+      </div>
+      <div className="m-5 flex justify-around items-center text-center">
+        
+        <button onClick={()=>setFoodId(food.id)} className="bg-green-500 border-none rounded-lg text-white text-sm font-semibold p-2.5 cursor-pointer">
+          View Recipe
+        </button>
+      </div>
     </div>
   );
 }
